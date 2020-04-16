c4384966506d9167f82a300118f315f48dcec572
GoBeSt <kjn582923@gmail.com>
2020-04-16 17:24:46 +0700

    gobest

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,40 @@
 package com.company;
 
+import com.sun.deploy.security.SelectableSecurityManager;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Scanner in = new Scanner(System.in);
         int x = in.nextInt();
-        int y = in.nextInt();
-        int z = in.nextInt();
-        if(z==0){ System.out.printf("%d",x+y);}
-        if(z==1){ System.out.printf("%d",x-y); }
-        if(z==2){ System.out.printf("%d",x*y); }
-        if(z==3) {
-            {if(y==0) System.out.println("cannot divide by zero");}
-        { System.out.printf("%f",(float)x/y); }
+        if (x >= 80) {
+            System.out.println("4");
         }
-        if(z==4){
-            {if(y==0) System.out.println("cannot divide by zero");}
-            {System.out.printf("%d",x%y);}
+        if (x >= 75) {
+            System.out.println("3.5");
         }
+        if (x >= 70) {
+            System.out.println("3");
+        }
+        if (x >= 65) {
+            System.out.println("2.5");
+        }
+        if (x >= 60) {
+            System.out.println("2");
+        }
+        if (x >= 55) {
+            System.out.println("1.5");
+        }
+        if (x >= 50) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+        ;
+
+
     }
 }
