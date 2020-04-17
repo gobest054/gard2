99cb213a578ae5164b66bb6b189b488f9b216d66
GoBeSt <kjn582923@gmail.com>
2020-04-17 16:32:24 +0700

    dabest

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,34 +7,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
         Scanner in = new Scanner(System.in);
-        int x = in.nextInt();
-        if (x >= 80) {
-            System.out.println("4");
-        }
-        else if (x >= 75) {
-            System.out.println("3.5");
-        }
-        else if (x >= 70) {
-            System.out.println("3");
-        }
-        else if (x >= 65) {
-            System.out.println("2.5");
-        }
-        else if (x >= 60) {
-            System.out.println("2");
-        }
-        else if (x >= 55) {
-            System.out.println("1.5");
-        }
-        else if (x >= 50) {
-            System.out.println("1");
-        } else {
-            System.out.println("0");
-        }
-        ;
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt();
+            if (x >= 80) {
+                System.out.println("4");
+            } else if (x >= 75) {
+                System.out.println("3.5");
+            } else if (x >= 70) {
+                System.out.println("3");
+            } else if (x >= 65) {
+                System.out.println("2.5");
+            } else if (x >= 60) {
+                System.out.println("2");
+            } else if (x >= 55) {
+                System.out.println("1.5");
+            } else if (x >= 50) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+            ;
 
 
+        }
     }
 }
